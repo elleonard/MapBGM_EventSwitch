93170b6564b8d1bc031014867690386c08d25241
plasma <librarian256@gmail.com>
2020-05-09 13:59:44 +0900

    code format

--- SoR_MapBGM_EventSwitch.js
+++ SoR_MapBGM_EventSwitch.js
@@ -59,93 +59,92 @@
  * -----------------------------------------------------------
  * v1.00 (2020/05/09)       公開  
  */
- /*:
- * @plugindesc ＜Conditional BGM switch on Map＞ While a specified switch is ON, BGM in a specified map is force changed.
- * @author Soryu @soryu_rpmaker
- *
- *
- * @help 
- * ------------------------------------------------------------
- * How to use
- * ------------------------------------------------------------
- * - Write note tag in your map which you want to change BGM
- *   with a condition as following format
- * <mapswitch: switchID, BGMname, volume, pitch,　pan, pos>
- *
- * Ex) In case of switch 10 is ON, Change BGM to Battle1 (volume and etc is as the default).
- * <mapswitch: 10, Battle1, 90, 100, 0, 0>
- *
- * Ex) In case of switch 33 is ON, Change BGM to Theme3 (pitch is 150%).
- * <mapswitch:33,Theme3,100,150,0,0>
- *
- * 
- * You can enumerate several "mapswitch" tags in a map.
- * If multiple conditions are hold, tags written earlier on 
- * the note has higher priority to apply. It is expected to express time flow
- * such as morning, noon, and night with different BGMs.
- *
- * To get more instructions, see https://github.com/soryu-rmv .
- *
- *
- * ------------------------------------------------------------
- * Version info
- * ------------------------------------------------------------
- * v1.00 (May 9, 2020)       Released!
- */
+/*:
+* @plugindesc ＜Conditional BGM switch on Map＞ While a specified switch is ON, BGM in a specified map is force changed.
+* @author Soryu @soryu_rpmaker
+*
+*
+* @help 
+* ------------------------------------------------------------
+* How to use
+* ------------------------------------------------------------
+* - Write note tag in your map which you want to change BGM
+*   with a condition as following format
+* <mapswitch: switchID, BGMname, volume, pitch,　pan, pos>
+*
+* Ex) In case of switch 10 is ON, Change BGM to Battle1 (volume and etc is as the default).
+* <mapswitch: 10, Battle1, 90, 100, 0, 0>
+*
+* Ex) In case of switch 33 is ON, Change BGM to Theme3 (pitch is 150%).
+* <mapswitch:33,Theme3,100,150,0,0>
+*
+* 
+* You can enumerate several "mapswitch" tags in a map.
+* If multiple conditions are hold, tags written earlier on 
+* the note has higher priority to apply. It is expected to express time flow
+* such as morning, noon, and night with different BGMs.
+*
+* To get more instructions, see https://github.com/soryu-rmv .
+*
+*
+* ------------------------------------------------------------
+* Version info
+* ------------------------------------------------------------
+* v1.00 (May 9, 2020)       Released!
+*/
+
+var So_Game_Map_setup = Game_Map.prototype.setup;
+Game_Map.prototype.setup = function (mapId) {
+  So_Game_Map_setup.call(this, mapId);
+  $dataMap.switchbgm_flagID = [];
+  $dataMap.switched_bgm = []; // switch bgm candidate array
+  this.setMapBGMtags();
+};
 
 
-    var So_Game_Map_setup = Game_Map.prototype.setup;
-    Game_Map.prototype.setup = function(mapId) {
-        So_Game_Map_setup.call(this, mapId);
-	    $dataMap.switchbgm_flagID = [];
-		$dataMap.switched_bgm = []; // switch bgm candidate array
-        this.setMapBGMtags();
-    };
-		
-	
-    Game_Map.prototype.setMapBGMtags = function() {
-	//mapswitch tag	
- 	var tag = /<(?:mapswitch):[ ]*(.*),[ ]*(.*),[ ]*(.*),[ ]*(.*),[ ]*(.*),[ ]*(.*)>/i;
+Game_Map.prototype.setMapBGMtags = function () {
+  //mapswitch tag	
+  var tag = /<(?:mapswitch):[ ]*(.*),[ ]*(.*),[ ]*(.*),[ ]*(.*),[ ]*(.*),[ ]*(.*)>/i;
 
-	 var notes = $dataMap.note.split(/[\r\n]+/);
-		for (var n = 0; n < notes.length; n++) {
-			  var line = notes[n];
-			  console.log(line);
-			  if (line.match(tag)) {
-				 var ID = parseInt(RegExp.$1);
-			     var switchbgm =  {
-					name: String(RegExp.$2),
-					volume: parseInt(RegExp.$3),
-					pitch: parseInt(RegExp.$4),
-					pan: parseInt(RegExp.$5),
-					pos: parseInt(RegExp.$6)
-				};
-				
-				$dataMap.switchbgm_flagID.push(ID);
-				$dataMap.switched_bgm.push(switchbgm);
-	  	     }
-		}
-    };
-	
-	//overwrite 
-	Game_Map.prototype.autoplay = function() {
-		if ($dataMap.autoplayBgm) {
-			if ($gamePlayer.isInVehicle()) {
-				$gameSystem.saveWalkingBgm2();
-			} else {
-				/////// conditional bgm play
-				if($dataMap.switched_bgm.length>0 ){
-					for(var i=0; i<$dataMap.switched_bgm.length;i++){
-						if($gameSwitches.value($dataMap.switchbgm_flagID[i])){
-						    AudioManager.playBgm($dataMap.switched_bgm[i]);
-							break;
-						}
-					}
-				}
-				else AudioManager.playBgm($dataMap.bgm); //regular bgm on database
-			}
-		}
-		if ($dataMap.autoplayBgs) {
-			AudioManager.playBgs($dataMap.bgs);
-		}
-	};
+  var notes = $dataMap.note.split(/[\r\n]+/);
+  for (var n = 0; n < notes.length; n++) {
+    var line = notes[n];
+    console.log(line);
+    if (line.match(tag)) {
+      var ID = parseInt(RegExp.$1);
+      var switchbgm = {
+        name: String(RegExp.$2),
+        volume: parseInt(RegExp.$3),
+        pitch: parseInt(RegExp.$4),
+        pan: parseInt(RegExp.$5),
+        pos: parseInt(RegExp.$6)
+      };
+
+      $dataMap.switchbgm_flagID.push(ID);
+      $dataMap.switched_bgm.push(switchbgm);
+    }
+  }
+};
+
+//overwrite 
+Game_Map.prototype.autoplay = function () {
+  if ($dataMap.autoplayBgm) {
+    if ($gamePlayer.isInVehicle()) {
+      $gameSystem.saveWalkingBgm2();
+    } else {
+      /////// conditional bgm play
+      if ($dataMap.switched_bgm.length > 0) {
+        for (var i = 0; i < $dataMap.switched_bgm.length; i++) {
+          if ($gameSwitches.value($dataMap.switchbgm_flagID[i])) {
+            AudioManager.playBgm($dataMap.switched_bgm[i]);
+            break;
+          }
+        }
+      }
+      else AudioManager.playBgm($dataMap.bgm); //regular bgm on database
+    }
+  }
+  if ($dataMap.autoplayBgs) {
+    AudioManager.playBgs($dataMap.bgs);
+  }
+};
